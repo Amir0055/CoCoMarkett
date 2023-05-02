e94e0e1928e48d64521d1a156222dd3d37e2e723
Aminesnoussi <mohamedamine.snoussi@esprit.tn>
2023-05-02 12:47:34 +0100

    snousi push 3

--- src/main/java/com/example/cocomarket/Controller/ProduitController.java
+++ src/main/java/com/example/cocomarket/Controller/ProduitController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/produits")
+
 public class ProduitController {
 
     @Autowired
--- src/main/java/com/example/cocomarket/Entity/Produit.java
+++ src/main/java/com/example/cocomarket/Entity/Produit.java
@@ -1,28 +1,31 @@
 package com.example.cocomarket.Entity;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 
 
 @Entity
 @Getter
 @Setter
-@ToString
+@ToString(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Produit {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @NonNull
+    @ToString.Include
     private Integer id;
     private String Reference;
+    @ToString.Include
     private String nom;
+    @ToString.Include
     private String img;
     private String description;
     private Float prix;
@@ -30,15 +33,28 @@ public class Produit {
     @Enumerated(EnumType.STRING)
     private Status status;//ywefe9 3lih lbaye3 bch ybi3ou ou non//par default Null
     private LocalDate datePublication;
-    @OneToMany(cascade = CascadeType.ALL)
-    private Set<Raiting_Product> raiting_prod;
+  //@JsonIgnore
+   // @OneToMany(cascade = CascadeType.ALL)
+   // private Set<Raiting_Product> raiting_prod;
+  @JsonIgnore
+  @ManyToMany(cascade = CascadeType.ALL)
+    private Set<Catalogue> Catalogues;
 
-
+@JsonIgnore
     @ManyToOne(cascade = CascadeType.ALL)
-    private Categorie Categories;
+    private Categorie categorie;
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Shop shop;
+@JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Raiting_Product> raiting_products ;
 
-    @ManyToOne
-    private Shop Shopes;
+    //@OneToMany(cascade = CascadeType.ALL)
+   // private Set<ImageData> image ;
+    @OneToMany(mappedBy = "produit",cascade = CascadeType.ALL)
+    private Set<ImageData> image ;
+
 
     @ManyToMany(mappedBy = "produits")
     private Set<Catalogue> catalogues = new HashSet<>();
@@ -47,25 +63,7 @@ public class Produit {
 
     private Integer pourcentagePromotion;
 
-    // Constructeurs
-    public Produit(String Reference, String nom, String img, String description, Float prix, Boolean EtatsProduit,
-                   Status status, LocalDate datePublication, Set<Raiting_Product> raiting_prod, Categorie Categories,
-                   Shop Shopes, Set<Catalogue> catalogues, Integer quantiteVendue, Integer pourcentagePromotion) {
-        this.Reference = Reference;
-        this.nom = nom;
-        this.img = img;
-        this.description = description;
-        this.prix = prix;
-        this.EtatsProduit = EtatsProduit;
-        this.status = status;
-        this.datePublication = datePublication;
-        this.raiting_prod = raiting_prod;
-        this.Categories = Categories;
-        this.Shopes = Shopes;
-        this.catalogues = catalogues;
-        this.quantiteVendue = quantiteVendue;
-        this.pourcentagePromotion = pourcentagePromotion;
-    }
+
 
 
 }
